f20dd20567bcedbf58e6fba978fd166f23b60f7a
markuszcl99 <e_markuszhang@163.com>
2023-02-23 22:47:14 +0800

    Semaphore 使用示例

---
+++ concurrent/src/main/java/com/markus/onjava/concurrent/SemaphoreDemo.java
@@ -0,0 +1,43 @@
+package com.markus.onjava.concurrent;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author: markus
+ * @date: 2023/2/23 10:34 PM
+ * @Description: Semaphore 使用示例
+ * @Blog: https://markuszhang.com
+ * @see Semaphore
+ * Java版的信号量，用于调控线程资源的数量，在一段时间内仅允许指定数量的线程执行，其余线程均被阻塞
+ * It's my honor to share what I've learned with you!
+ */
+public class SemaphoreDemo {
+    private static class Taxi implements Runnable {
+        private final Semaphore semaphore;
+
+        public Taxi(Semaphore semaphore) {
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire();
+                Thread.sleep(1000);
+                System.out.println("Taxi [" + Thread.currentThread().getId() + "] pickup finished");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new Taxi(semaphore));
+            thread.start();
+        }
+    }
+}
