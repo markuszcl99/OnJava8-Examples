a8a090eb2b3cfb8ffa46a6c6c1c063498d10f4e1
markuszcl99 <e_markuszhang@163.com>
2023-03-01 21:38:00 +0800

    JavaRed框架学习

---
+++ concurrent/src/main/java/com/markus/onjava/concurrent/red/JavaRed.java
@@ -0,0 +1,11 @@
+package com.markus.onjava.concurrent.red;
+
+/**
+ * @author: markus
+ * @date: 2023/3/1 9:37 PM
+ * @Description:
+ * @Blog: https://markuszhang.com
+ * It's my honor to share what I've learned with you!
+ */
+public class JavaRed {
+}
