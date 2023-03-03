262ec5fdf08a87d3816507593959ba3eebfd8fc7
markuszcl99 <e_markuszhang@163.com>
2023-03-03 23:33:52 +0800

    异步任务框架代码

---
+++ concurrent/src/main/java/com/markus/onjava/concurrent/task/framework/AsyncTask.java
@@ -0,0 +1,11 @@
+package com.markus.onjava.concurrent.task.framework;
+
+/**
+ * @author: markus
+ * @date: 2023/3/3 11:33 PM
+ * @Description: 异步任务
+ * @Blog: https://markuszhang.com
+ * It's my honor to share what I've learned with you!
+ */
+public class AsyncTask {
+}
