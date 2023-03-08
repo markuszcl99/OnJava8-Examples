42c7b59067541c3bf36f75463b2e2e8f4c21ce0f
markuszcl99 <e_markuszhang@163.com>
2023-03-08 22:29:46 +0800

    异步任务框架代码-随便提交一下

--- concurrent/src/main/java/com/markus/onjava/concurrent/task/framework/AsyncTaskThreadPool.java
+++ concurrent/src/main/java/com/markus/onjava/concurrent/task/framework/AsyncTaskThreadPool.java
@@ -2,11 +2,10 @@ package com.markus.onjava.concurrent.task.framework;
 
 /**
  * @author: markus
- * @date: 2023/3/3 11:33 PM
- * @Description: 异步任务
+ * @date: 2023/3/8 8:40 AM
+ * @Description: 异步任务线程池
  * @Blog: https://markuszhang.com
  * It's my honor to share what I've learned with you!
  */
-public class AsyncTask {
-
+public class AsyncTaskThreadPool {
 }
